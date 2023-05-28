b7ec673c2e6d14c937afcce49a36bc2a03ead978
pgeebansko <pgee_students@abv.bg>
2023-05-28 22:23:26 +0300

    25052

--- tools.py
+++ tools.py
@@ -1,17 +1,10 @@
 import os
 import glob
 
-# get all files inside a specific folder
-dir_path = r'C:\ggb\GEORGI\DZI_22_23'
-for path in os.scandir(dir_path):
-    if path.is_file():
-        print(path.name)
-
-
 # search all files inside a specific folder
 # *.* means file name with any extension
-dir_path = r'C:\ggb\GEORGI\DZI_22_23\*.*'
+dir_path = r'C:\ggb\GEORGI\DZI_22_23\projects\arichiver\*.txt'
 res = glob.glob(dir_path, recursive=True)
 print(res)
 for path in res:
-    print(path)
+    print('*****'+path)
